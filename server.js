52fb77256f70c73d6108081a89d052e98460dac8
Cees <cjol99@icloud.com>
2021-06-23 10:22:16 +0200

    Add router and POST request logic

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const bodyParser = require("body-parser"); // Middleware for POST requests
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // this is the in-memory database ;)
 let counter = 0;
 
-app.get('/api/data', function (req, res) {
+app.get("/api/data", function (req, res) {
   counter++;
-  return res.json({name: 'sunshine', counter: counter});
+  return res.json({ name: "sunshine", counter: counter });
+});
+
+app.post("/api/new", (req, res) => {
+  //code to perform particular action.
+  //To access POST variable use req.body()methods.
+  console.log(req.body);
+
+  // Return ok (probably a better way to do this ...)
+  return res.json({ ok: "ok" });
 });
 
 app.listen(process.env.PORT || 8080);
---
+++ src/components/New.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+const New = () => {
+  const [clientName, setClientName] = useState("");
+  const handleChangeClientName = (event) => {
+    setClientName(event.target.value);
+  };
+  const handleCreate = async () => {
+    // Should validate input
+    // ...
+
+    // Post the request
+    const data = await fetch(`/api/new`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ clientName }),
+    });
+    const json = await data.json();
+    console.log("json:", json);
+  };
+  return (
+    <div>
+      <h2>New Care Request</h2>
+      <form>
+        <label>Client Name: </label>
+        <input
+          type="text"
+          id="clientName"
+          name="clientName"
+          value={clientName}
+          onChange={handleChangeClientName}
+          // placeholder="Client Name"
+        />
+      </form>
+
+      <button onClick={handleCreate}>Post request</button>
+    </div>
+  );
+};
+
+export default New;
