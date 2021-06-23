aec856dd7baae8e034ef6d644dfee4106ad14fb6
Cees <cjol99@icloud.com>
2021-06-23 11:49:23 +0200

    Add apply to request functionality

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ let guid = () => {
 };
 
 // TODO remove default request... that's just temporary
-const careRequests = [
+let careRequests = [
   {
     clientName: "John SOme",
     status: "OPEN",
@@ -48,13 +48,14 @@ const careRequests = [
 ];
 
 /**
- * Return the open care requests.
+ * Return the care requests. Return only the requests where status equals "OPEN".
  * Type: GET
  */
 app.get("/api/open-requests", function (req, res) {
-  // TODO: filter care requests: return the 'open' ones only (simple property)
   // TODO dont return all details, just some important things for the overview page
-  return res.json({ careRequests });
+  return res.json({
+    careRequests: careRequests.filter((careReq) => careReq.status === "OPEN"),
+  });
 });
 
 /**
@@ -62,11 +63,15 @@ app.get("/api/open-requests", function (req, res) {
  * Type: GET
  */
 app.get("/api/request/:id", function (req, res) {
+  // Find request
   const careRequest = careRequests.filter(
     (careReq) => careReq.id === req.params.id
   )[0];
   console.log("careRequest:", careRequest);
+
   // TODO handle empty careRequest (careRequest not found)
+  // ...
+
   return res.json({ careRequest });
 });
 
@@ -85,6 +90,33 @@ app.post("/api/new", (req, res) => {
 
   // Store careRequest
   careRequests.push(careRequest);
+
+  // Return ok (probably a better way to do this ...)
+  return res.json({ ok: "ok" });
+});
+
+/**
+ * API that allows caregiver to apply to a request.
+ * Type: POST
+ * TODO could just be get...? Not sure
+ */
+app.post("/api/apply/:id", (req, res) => {
+  console.log(req.body);
+
+  // Find request and close it
+  careRequests = careRequests.map((careReq) => {
+    if (careReq.id === req.params.id) {
+      return {
+        ...careReq,
+        status: "CLOSED",
+      };
+    }
+
+    return careReq;
+  });
+
+  // TODO handle empty careRequest (careRequest not found)
+  // ...
 
   // Return ok (probably a better way to do this ...)
   return res.json({ ok: "ok" });
--- src/components/Request.js
+++ src/components/Request.js
@@ -4,11 +4,25 @@ import { useParams } from "react-router-dom";
 const Request = () => {
   const [careRequest, setCareRequest] = useState({});
   const { id } = useParams(); // TODO i hope this is defined before the useEffect call...
+  const [applyState, setApplyState] = useState("");
   const fetchRequest = async () => {
     // Fetch all request details using the ID
     const response = await fetch(`/api/request/${id}`);
     const result = await response.json();
     setCareRequest(result.careRequest);
+  };
+  const handleApply = async () => {
+    setApplyState("LOADING");
+    const data = await fetch(`/api/apply/${id}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({}),
+    });
+    const json = await data.json();
+    console.log("json:", json);
+    setApplyState("SUCCESS");
   };
   useEffect(() => {
     // Request open care requests on load
@@ -20,27 +34,48 @@ const Request = () => {
   }
   return (
     <div>
-      <h2>Request details</h2>
-      <p>
-        Kind of care needed:
-        <br /> <b>{careRequest.kind}</b>
-      </p>
-      <p>
-        Start date and time: <br />
-        <b>{careRequest.startDateTime}</b>
-      </p>
-      <p>
-        End date and time: <br />
-        <b>{careRequest.endDateTime}</b>
-      </p>
-      <p>
-        Client name: <br />
-        <b>{careRequest.clientName}</b>
-      </p>
-      <p>
-        Status: <br />
-        <b>{careRequest.status}</b>
-      </p>
+      <div className="request-info">
+        <h2>Request details</h2>
+        <p>
+          Kind of care needed:
+          <br /> <b>{careRequest.kind}</b>
+        </p>
+        <p>
+          Start date and time: <br />
+          <b>{careRequest.startDateTime}</b>
+        </p>
+        <p>
+          End date and time: <br />
+          <b>{careRequest.endDateTime}</b>
+        </p>
+        <p>
+          Client name: <br />
+          <b>{careRequest.clientName}</b>
+        </p>
+        <p>
+          Status: <br />
+          <b>{careRequest.status}</b>
+        </p>
+      </div>
+
+      <hr />
+
+      <div className="actions">
+        <h2>Apply</h2>
+        {applyState === "" && careRequest.status === "OPEN" && (
+          <form>
+            <label>You can apply to this request using this button.</label>
+            <button onClick={handleApply}>Apply</button>
+          </form>
+        )}
+        {applyState === "" && careRequest.status === "CLOSED" && (
+          <p>This request is closed. You cannot apply to it.</p>
+        )}
+        {applyState === "LOADING" && <p>Applying...</p>}
+        {applyState === "SUCCESS" && (
+          <p>Applied successfully! The care request has been closed.</p>
+        )}
+      </div>
     </div>
   );
 };
