4a5480a77b56ce33a0c889bea2d7c0926aaa621e
Cees <cjol99@icloud.com>
2021-06-23 11:21:45 +0200

    Add kind of care and extra information input

--- src/components/New.js
+++ src/components/New.js
@@ -1,9 +1,17 @@
 import React, { useState } from "react";
 
 const New = () => {
+  const [kind, setKind] = useState("household");
   const [clientName, setClientName] = useState("");
+  const [extraInfo, setExtraInfo] = useState("");
+  const handleChangeKind = (event) => {
+    setKind(event.target.value);
+  };
   const handleChangeClientName = (event) => {
     setClientName(event.target.value);
+  };
+  const handleChangeExtraInfo = (event) => {
+    setExtraInfo(event.target.value);
   };
   const handleCreate = async () => {
     // Should validate input
@@ -15,7 +23,7 @@ const New = () => {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ clientName }),
+      body: JSON.stringify({ kind, clientName, extraInfo }),
     });
     const json = await data.json();
     console.log("json:", json);
@@ -27,14 +35,34 @@ const New = () => {
     <div>
       <h2>New Care Request</h2>
       <form>
-        <label>Client Name: </label>
+        <label>Kind of care needed: </label>
+        <select
+          className="select-narrow"
+          name={"kind"}
+          id={kind}
+          value={kind}
+          onChange={handleChangeKind}
+        >
+          <option value="household">Household</option>
+          <option value="medical">Medical</option>
+        </select>
+
+        <label>Client name: </label>
         <input
           type="text"
           id="clientName"
           name="clientName"
           value={clientName}
           onChange={handleChangeClientName}
-          // placeholder="Client Name"
+        />
+
+        <label>Extra information: </label>
+        <textarea
+          type="extraInfo"
+          id="extraInfo"
+          name="extraInfo"
+          value={extraInfo}
+          onChange={handleChangeExtraInfo}
         />
       </form>
 
